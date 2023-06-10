eee01eb0bb1b4594f7feb4b74624e6851b27b069
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-06-10 16:18:48 +0600

    object injs

---
+++ object.js
@@ -0,0 +1,37 @@
+// create object using object literals
+const player = {};
+player.name = "Fahim";
+player.speciality = "ALlraUNder";
+player.bat = function () {
+    console.log('swing your bat');
+}
+console.log(player);
+player.bat();
+
+const student = {
+    name: "paddey",
+    job: "pada",
+    ball: function () {
+        console.log("ne an")
+    },
+}
+
+const person = new Object();
+
+// 3
+const item = Object.create(null);
+console.log(item);
+
+
+// =================================================
+// =======using CLass and more importtant this======
+// =================================================
+class Person{
+    name = "abul";
+    address = 'sodor ghat';
+    constructor(age) {
+        this.age = age;
+    }
+}
+const person1 = new Person(56);
+console.log(person1);
